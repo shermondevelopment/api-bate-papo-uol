2531df3ba2acc161e0f39f81f98adc460729c1d6
shermondevelopment <victorshermon@yahoo.com>
2022-04-26 20:30:40 -0300

    including lib dotenv in project

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
+import 'dotenv/config'
 import express from 'express'
 
 const app = express()
 
 
-app.listen(process.env.PORT || 5000, console.log(`appp running in port ${process.env.PORT} 🚀🚀🚀🚀`))
+app.listen(process.env.PORT || 5000, console.log(`app running in port ${process.env.PORT} 🚀🚀🚀🚀`))
