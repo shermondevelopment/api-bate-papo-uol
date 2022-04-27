369e7797dd40b7ee0f2ae0dbde9cec1e0932fd95
shermondevelopment <victorshermon@yahoo.com>
2022-04-27 18:58:36 -0300

    connect in mongodb before of start the apllication

--- app/index.js
+++ app/index.js
@@ -1,7 +1,11 @@
 import 'dotenv/config'
 import express from 'express'
+import connect from './setting/mongoose.js'
 
 const app = express()
 
 
-app.listen(process.env.PORT || 5000, console.log(`app running in port ${process.env.PORT} 🚀🚀🚀🚀`))
+connect.then( connected => {
+  app.listen(process.env.PORT || 5000, console.log(`app running in port ${process.env.PORT} 🚀🚀🚀🚀`))
+} )
+
