b695f8081b32c4e45ad9f6011e7ecca06b2d24c2
shermondevelopment <victorshermon@yahoo.com>
2022-04-27 19:43:14 -0300

    turning the request body into a json

--- app/index.js
+++ app/index.js
@@ -1,9 +1,9 @@
 import 'dotenv/config'
-import express from 'express'
+import express, { json } from 'express'
 import connect from './setting/mongoose.js'
 
 const app = express()
-
+app.use(json)
 
 connect.then( connected => {
   app.listen(process.env.PORT || 5000, console.log(`app running in port ${process.env.PORT} 🚀🚀🚀🚀`))
