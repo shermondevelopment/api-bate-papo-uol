56235cbd2a542130201e3eac8fc500a2c4d5446c
shermondevelopment <victorshermon@yahoo.com>
2022-04-27 20:01:00 -0300

    including settings of cors

--- app/index.js
+++ app/index.js
@@ -1,10 +1,20 @@
 import 'dotenv/config'
+
+/* cors */
+import cors from 'cors'
+
+/* express */
 import express, { json } from 'express'
+
+/* mongodb connect */
 import connect from './setting/mongoose.js'
 
+/* server */
 const app = express()
-app.use(json)
+app.use(json())
+app.use(cors())
 
+/* start app */
 connect.then( connected => {
   app.listen(process.env.PORT || 5000, console.log(`app running in port ${process.env.PORT} 🚀🚀🚀🚀`))
 } ).catch(() => console.log('Ops você não tem uma conexão com banco...😵😵❌❌'))
