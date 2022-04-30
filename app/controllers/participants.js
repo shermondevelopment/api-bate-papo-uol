de4cb36750f7547620fd7fb3532407f0350ce6c9
shermondevelopment <victorshermon@yahoo.com>
2022-04-29 22:30:54 -0300

    saving mesage of input of users

--- app/controllers/participants.js
+++ app/controllers/participants.js
@@ -3,18 +3,25 @@ import { participantSchema } from '../validation/index.js'
 /* model participants */ 
 import participantsModel from '../schema/participants.js'
 
+/* messages model */
+import messageModel from '../schema/messages.js'
+
+/* datjs */
+import dayjs from 'dayjs'
+
 export const participantsAdd = async (req, res) => {
   try {
     await participantSchema.validateAsync(req.body)
-
-    const findParticipant = await participantsModel.findOne({ where: req.body.name})
-
+    const findParticipant = await participantsModel.findOne({ name: req.body.name})
     if(findParticipant) {
       return res.status(409).send('o usuário já existe')
     }
 
+    await messageModel.create({ from: 'xxx', to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs(new Date()).format('HH:mm:ss') })
+
+
     await participantsModel.create(req.body)
-    res.status(201).json(req.body.name)
+    res.sendStatus(201)
   } catch (error) {
     res.status(422).json(error.message)
   }
